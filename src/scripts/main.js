9bc0536edef70712a56be066c04fc90bfd0e2b68
Mitch <hello@liquidvisual.com>
2015-03-09 12:58:41 +1100

    Tweaking video bg fallbacj

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -36,6 +36,23 @@ $(window).load(function() {
     // $('.lv-scroller').mCustomScrollbar("destroy");
 // });
 
+// $(function() {
+
+//     // onload
+//     if(document.body.clientWidth >= 870) {
+//         $('video').attr('autoplay', true);
+//     }
+
+//     // If you want to autoplay when the window resized wider than 780px
+//     // after load, you can add this:
+
+//     $(window).resize(function() {
+//         if(document.body.clientWidth >= 870) {
+//             $('video').attr('autoplay', true);
+//         }
+//     });
+// });
+
 //-----------------------------------------------------------------
 //
 //-----------------------------------------------------------------
